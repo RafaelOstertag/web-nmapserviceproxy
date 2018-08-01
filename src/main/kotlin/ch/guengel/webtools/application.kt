8fd5eb2fb85c914568c7a74a7cb98d4808b060db
Rafael Ostertag <rafi@guengel.ch>
2018-08-01 16:30:37 +0200

    Basic working version of application.kt

--- src/main/kotlin/ch/guengel/webtools/application.kt
+++ src/main/kotlin/ch/guengel/webtools/application.kt
@@ -1,11 +1,13 @@
 package ch.guengel.webtools
 
 import ch.guengel.webtools.services.LastSeenService
+import ch.guengel.webtools.services.ScanService
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
 import org.slf4j.LoggerFactory
 
 private val lastSeenService = LastSeenService("gizmo.kruemel.home", 40001)
+private val scanService = ScanService("gizmo.kruemel.home", 40000)
 private val logger = LoggerFactory.getLogger("application")
 
 private fun errorResponse(message: String?): String {
@@ -21,24 +23,36 @@ fun main(args: Array<String>) {
         addRoute("/v1/scan/:ip") { routingContext ->
             val ipRequestingScan = routingContext.getClientIp()
             val ipToScan = routingContext.pathParam("ip")
+
             logger.info("Inquiry number of occurrences for {}", ipRequestingScan)
-            lastSeenService.isIpWithinTimeConstraint(ipRequestingScan).setHandler {
+
+            lastSeenService.isIpWithinTimeConstraint(ipRequestingScan).compose { success ->
+                if (!success) {
+                    throw HttpException(400, "Too many scans")
+                }
+                scanService.scanHost(ipToScan)
+            }.setHandler {
                 when {
-                    it.failed() -> routingContext
+                    it.failed() -> {
+                        routingContext
+                            .response()
+                            .contentTypeJson()
+
+                        val exception = it.cause()
+                        if (exception is HttpException) {
+                            routingContext
+                                .response()
+                                .setStatusCode(exception.statusCode)
+                        } else {
+                            routingContext.response().setStatusCode(500)
+                        }
+                            .end(errorResponse(it.cause().message))
+                    }
+                    else -> routingContext
                         .response()
                         .contentTypeJson()
-                        .setStatusCode(500)
-                        .setStatusMessage("Internal server error")
-                        .end(errorResponse(it.cause().message))
-                    it.result() == true -> routingContext
-                        .response()
                         .setStatusCode(200)
-                        .end("Would now send scan request for $ipToScan")
-                    it.result() == false -> routingContext
-                        .response()
-                        .contentTypeJson()
-                        .setStatusCode(400)
-                        .end(errorResponse("Too many scans"))
+                        .end(it.result())
                 }
             }
         }
